a038f47235296544a8ba3fbb9bc43e67a02d70c7
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-02-21 09:22:18 +0100

    add solution to duplicate arguments task

---
+++ 6kyu/duplicate_arguments.py
@@ -0,0 +1,8 @@
+def solution(*args):
+    lst = []
+    for i in range(len(args)):
+        if args[i] not in lst:
+            lst.append(args[i])
+        else:
+            return True
+    return False
